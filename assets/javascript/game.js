54cf554af67fbd95da7c0f2bcccedaf07c0c1b15
Achea Compton <acheacompton@Acheas-MacBook-Pro.local>
2017-08-12 23:08:30 -0700

    bugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var wins = 0;
-var looses = 0;
-var guessTotal = 0;
+var losses = 0;
+var currentTotal = 0;
+
 
 document.onkey = function(event) {
  
@@ -10,38 +11,66 @@ var april = Math.floor((Math.random()*12)+1);
 var october = Math.floor((Math.random()*12)+1);
 var december = Math.flor((Math.random()*12)+1);
 
-$ (".numberdisplay").html(number);
+$ ("#numberDisplay").html(number);
 
-$ (".julycrystal").click(function()) {
-	update(july)};
+$ (".julycrystal").on("click", function(){
+	update(july)
+});
 
-$ (".aprilcrystal").click(function()){
-	update(april)};
+$ (".aprilcrystal").on("click", function() {
+	update(april)
+});
 
-$ (".octobercrystal").click(function()) {
-	update(october)};
+$ (".octobercrystal").on("click", function() {
+	update(october)
+});
 
-$ (".decembercrystal").click(function()) {
-	update(december)};
+$ (".decembercrystal").on("click", function() {
+	update(december)
+});
 
 function reset () {
 	
 	var number = Math.floor((Math.random()*120) + 19);
 
-	$ (".numberdisplay").html(number);
+	$ ("#numberDisplay").html(number);
 
 	var july = Math.floor((Math.random()*12) +1);
 	var april = Math.floor((Math.random()*12)+1);
 	var october = Math.floor((Math.random()*12)+1);
  	var december = Math.flor((Math.random()*12)+1);
 
- 	guessTotal =0 ;
+ 	currentTotal =0 ;
 
- 	$ (".guessTotal").html(guessTotal);
+ 	$ ("#currentTotal").html(currentTotal);
+}
+
+function update(color) {
+
+	currentTotal += color;
+
+	currentTotal = 0;
+	
+	$ ("#currentDisplay").append(currentTotal);
+
+	if (currentTotal > number)
+		losses++;
+		$("#lossesDisplay").html(losses);
+
+	reset();
+
+	if (currentTotal === number)
+		wins++;
+		$("#winsDisplay").html(losses);
+
+	reset();
+}
+
 }
 
 
 
 
 
-}
+
+
