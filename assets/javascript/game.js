a1255bb9f27f640ac29987fe673eb90ef1e2dc9e
Achea Compton <acheacompton@Acheas-MacBook-Pro.local>
2017-08-12 20:43:02 -0700

    second draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,45 @@ var looses = 0;
 var guessTotal = 0;
 
 document.onkey = function(event) {
+ 
+var number = Math.floor((Math.random()*120) + 19);
+var july = Math.floor((Math.random()*12) +1);
+var april = Math.floor((Math.random()*12)+1);
+var october = Math.floor((Math.random()*12)+1);
+var december = Math.flor((Math.random()*12)+1);
+
+$ (".numberdisplay").html(number);
+
+$ (".julycrystal").click(function()) {
+	update(july)};
+
+$ (".aprilcrystal").click(function()){
+	update(april)};
+
+$ (".octobercrystal").click(function()) {
+	update(october)};
+
+$ (".decembercrystal").click(function()) {
+	update(december)};
+
+function reset () {
+	
+	var number = Math.floor((Math.random()*120) + 19);
+
+	$ (".numberdisplay").html(number);
+
+	var july = Math.floor((Math.random()*12) +1);
+	var april = Math.floor((Math.random()*12)+1);
+	var october = Math.floor((Math.random()*12)+1);
+ 	var december = Math.flor((Math.random()*12)+1);
+
+ 	guessTotal =0 ;
+
+ 	$ (".guessTotal").html(guessTotal);
+}
+
+
+
 
 
 }
